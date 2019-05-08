f061d878e97081a9f5f479d67a93612a1609efd4
Cesar De la Torre Llorente <cesardl@microsoft.com>
2019-05-07 21:46:48 -0700

    Added BULK PREDICTIONS for Test Cases

--- MLModel.Train/UnitTests/MLModelUnitTests.cs
+++ MLModel.Train/UnitTests/MLModelUnitTests.cs
@@ -4,6 +4,8 @@ using System.IO;
 
 using Microsoft.ML;
 using SentimentModel.Model.DataModels;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Tests
 {
@@ -14,6 +16,8 @@ namespace Tests
 
         //Machine Learning model to load and use for predictions
         private const string MODEL_FILEPATH = @"../../../../SentimentModel/SentimentModel.Model/MLModel.zip";
+
+        private const string TEST_DATA_FILEPATH = @"../../../test_data.tsv";
 
         [SetUp]
         public void Setup()
@@ -46,6 +50,55 @@ namespace Tests
             var resultprediction = predEngine.Predict(sampleStatement);
 
             Assert.AreEqual(false, Convert.ToBoolean(resultprediction.Prediction));
+        }
+
+        //Generate many test cases with a bulk prediction approach
+        public static List<TestCaseData> TestCases
+        {
+            get
+            {
+                MLContext mlContext = new MLContext();
+                ITransformer trainedModel = mlContext.Model.Load(GetAbsolutePath(MODEL_FILEPATH), out var modelInputSchema);
+
+                // Read dataset to get a single row for trying a prediction          
+                IDataView testDataView = mlContext.Data.LoadFromTextFile<ModelInput>(
+                                                path: GetAbsolutePath(TEST_DATA_FILEPATH),
+                                                hasHeader: true,
+                                                separatorChar: '\t');
+
+                IEnumerable<ModelInput> samplesForPrediction = mlContext.Data.CreateEnumerable<ModelInput>(testDataView, false);
+                ModelInput[] arraysamplesForPrediction = samplesForPrediction.ToArray();
+
+                //DO BULK PREDICTIONS
+                IDataView predictionsDataView = trainedModel.Transform(testDataView);
+                IEnumerable<ModelOutput> predictions = mlContext.Data.CreateEnumerable<ModelOutput>(predictionsDataView, false);
+                ModelOutput[] arrayPredictions = predictions.ToArray();
+
+                var TestCases = new List<TestCaseData>();
+
+                for (int i = 0; i < arraysamplesForPrediction.Length; i++)
+                {
+                    TestCases.Add(new TestCaseData(arraysamplesForPrediction[i].Text,
+                                                   arrayPredictions[i].Prediction,
+                                                   arraysamplesForPrediction[i].Sentiment));
+                }
+
+                return TestCases;
+            }
+        }
+
+        [TestCaseSource("TestCases")]
+        public void TestSentimentStatement(string sampleText, bool predictedSentiment, bool expectedSentiment)
+        {
+            try
+            {
+                Console.WriteLine($"Text {sampleText} predicted as {predictedSentiment} should be {expectedSentiment}");
+                Assert.AreEqual(predictedSentiment, expectedSentiment);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public static string GetAbsolutePath(string relativePath)
