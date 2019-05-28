c4a5f961f59bedbb691953a142b941124dabfe19
Cesar De la Torre Llorente <cesardl@microsoft.com>
2019-05-28 11:45:48 -0700

    Train on MSFT-hosted agent & small dataset in Git

--- MLModel.Train/SentimentModel/SentimentModel.ConsoleApp/ModelBuilder.cs
+++ MLModel.Train/SentimentModel/SentimentModel.ConsoleApp/ModelBuilder.cs
@@ -17,11 +17,10 @@ namespace SentimentModel.ConsoleApp
 {
     public static class ModelBuilder
     {
-        // SMALL DATASET in GitHub repo: private const string TRAIN_DATA_FILEPATH = @"../../../../../Data/yelp_labelled.tsv";
-        // SMALL DATASET in AZURE FILES: private static string TRAIN_DATA_FILEPATH = @"X:\\yelp\\yelp_labelled.tsv";
-        
-        // LARGE DATASET in AZURE FILES: 
-        private static string TRAIN_DATA_FILEPATH = @"X:\\twitter\\Twittersentiment-1Million.tsv";
+        // SMALL DATASET in GitHub repo: 
+        private const string TRAIN_DATA_FILEPATH = @"../../../../../Data/yelp_labelled.tsv";    
+        // SMALL DATASET in AZURE FILES: private static string TRAIN_DATA_FILEPATH = @"X:\\yelp\\yelp_labelled.tsv";      
+        // LARGE DATASET in AZURE FILES: private static string TRAIN_DATA_FILEPATH = @"X:\\twitter\\Twittersentiment-1Million.tsv";
 
         private static string MODEL_FILEPATH = @"../../../../SentimentModel.Model/MLModel.zip";
 
